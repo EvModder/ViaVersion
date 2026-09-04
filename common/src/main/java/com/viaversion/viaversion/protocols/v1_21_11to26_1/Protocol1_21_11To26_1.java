4890c904b49df4eb07129ecea9f0dda315949aba
Nassim Jahnke <nassim@njahnke.dev>
2026-09-04 20:45:42 +0200

    Remove pointless tests

--- common/src/main/java/com/viaversion/viaversion/protocols/v1_21_11to26_1/Protocol1_21_11To26_1.java
+++ common/src/main/java/com/viaversion/viaversion/protocols/v1_21_11to26_1/Protocol1_21_11To26_1.java
@@ -144,8 +144,13 @@ public final class Protocol1_21_11To26_1 extends AbstractProtocol<ClientboundPac
             addBabyAssetId(tag);
         });
         registryDataRewriter.addHandler("dimension_type", (key, tag) -> {
-            tag.putBoolean("has_ender_dragon_fight", Key.equals(key, "the_end"));
-            addLegacyDefaultClock(key, tag);
+            final String strippedKey = Key.stripMinecraftNamespace(key);
+            tag.putBoolean("has_ender_dragon_fight", strippedKey.equals("the_end"));
+
+            switch (strippedKey) {
+                case "overworld", "overworld_caves" -> tag.putString("default_clock", "minecraft:overworld");
+                case "the_end" -> tag.putString("default_clock", "minecraft:the_end");
+            }
 
             CompoundTag attributes = tag.getCompoundTag("attributes");
             if (attributes == null) {
@@ -153,7 +158,7 @@ public final class Protocol1_21_11To26_1 extends AbstractProtocol<ClientboundPac
                 tag.put("attributes", attributes);
             }
 
-            final int ambientLightColor = switch (Key.stripMinecraftNamespace(key)) {
+            final int ambientLightColor = switch (strippedKey) {
                 case "the_end" -> -12630209;
                 case "the_nether" -> -13621215;
                 case "overworld" -> -16119286;
@@ -166,7 +171,9 @@ public final class Protocol1_21_11To26_1 extends AbstractProtocol<ClientboundPac
 
             final long dayTime = wrapper.read(Types.LONG);
             final boolean tickDayTime = wrapper.read(Types.BOOLEAN);
-            writeLegacyClockUpdates(wrapper, dayTime, tickDayTime);
+            wrapper.write(Types.VAR_INT, 2);
+            writeClockUpdate(wrapper, 0, dayTime, tickDayTime); // Overworld clock
+            writeClockUpdate(wrapper, 1, dayTime, tickDayTime); // The End clock
         });
 
         replaceClientbound(ClientboundPackets1_21_11.UPDATE_TAGS, this::handleTags);
@@ -181,24 +188,11 @@ public final class Protocol1_21_11To26_1 extends AbstractProtocol<ClientboundPac
         });
     }
 
-    static void writeLegacyClockUpdates(final PacketWrapper wrapper, final long dayTime, final boolean tickDayTime) {
-        wrapper.write(Types.VAR_INT, 2);
-        writeClockUpdate(wrapper, 0, dayTime, tickDayTime); // Overworld clock
-        writeClockUpdate(wrapper, 1, dayTime, tickDayTime); // The End clock
-    }
-
-    private static void writeClockUpdate(final PacketWrapper wrapper, final int clockId, final long dayTime, final boolean tickDayTime) {
+    private void writeClockUpdate(final PacketWrapper wrapper, final int clockId, final long dayTime, final boolean tickDayTime) {
         wrapper.write(Types.VAR_INT, clockId);
         wrapper.write(Types.VAR_LONG, dayTime);
         wrapper.write(Types.FLOAT, 0F);
         wrapper.write(Types.FLOAT, tickDayTime ? 1F : 0F);
-    }
-
-    static void addLegacyDefaultClock(final String dimensionTypeKey, final CompoundTag tag) {
-        switch (Key.stripMinecraftNamespace(dimensionTypeKey)) {
-            case "overworld", "overworld_caves" -> tag.putString("default_clock", "minecraft:overworld");
-            case "the_end" -> tag.putString("default_clock", "minecraft:the_end");
-        }
     }
 
     private void handleTags(final PacketWrapper wrapper) {
